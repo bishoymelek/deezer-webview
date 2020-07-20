039a5ae66b1881f7434ace7b7cd6f112a99b9802
Bishoy <bishoymelekwadie@gmail.com>
2020-07-20 05:28:48 +0200

    add styled components pkg, styled genreCard and Spinner,update tests

--- src/components/GenreCard.tsx
+++ src/components/GenreCard.tsx
@@ -1,19 +1,35 @@
 import React from 'react';
 import { Card } from 'antd';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
-type GenreCardPropType = { name: string; id: string; picture: string };
+type GenreCardPropType = {
+  name: string;
+  id: string;
+  picture: string;
+  className?: string;
+};
 
-export default function GenreCard(props: GenreCardPropType): JSX.Element {
-  const { picture, name, id } = props;
+function GenreCard(props: GenreCardPropType): JSX.Element {
+  const { picture, name, id, className } = props;
   return (
     <Link to={`/genres/${id}`}>
       <Card
-        className="genre-card"
+        className={`genre-card ${className}`}
         hoverable
         title={name}
-        cover={<img alt="example" src={picture} />}
+        cover={<img alt="genre" src={picture} />}
       />
     </Link>
   );
 }
+
+const StyledGenreCard = styled(GenreCard)`
+  &.genre-card {
+    .ant-card-head-title {
+      text-align: center;
+    }
+  }
+`;
+
+export default StyledGenreCard;
--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -1,16 +1,41 @@
 import React from 'react';
 import { Spin } from 'antd';
+import styled from 'styled-components';
 
-function Spinner(props: any): JSX.Element {
-  const { spinning, delay = '1000' } = props;
+type SpinnerPropType = { spinning: boolean; delay?: number; className: string };
+
+function Spinner(props: SpinnerPropType): JSX.Element {
+  const { spinning, delay = 1000, className } = props;
   if (spinning) {
     return (
-      <div className="spinner-wrapper">
+      <div className={`spinner-wrapper ${className}`}>
         <Spin spinning size="large" wrapperClassName="comp" delay={delay} />
       </div>
     );
   }
   return <></>;
 }
-export default Spinner;
-export { Spinner };
+const StyledSpinner = styled(Spinner)`
+  &.spinner-wrapper {
+    width: 100%;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    position: fixed;
+    background-color: rgba(255, 255, 255, 0.7);
+    z-index: 99999;
+
+    .ant-spin {
+      z-index: 9991;
+      margin: auto;
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+    }
+  }
+`;
+
+export default StyledSpinner;
+export { StyledSpinner };
